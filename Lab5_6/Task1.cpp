138b159b8fa331cedc15fd5af2191806d7f2519c
Wajahat Ali Basharat <83087637+WajahatAliBasharat073@users.noreply.github.com>
2022-11-30 18:06:00 +0500

    Add files via upload

---
+++ Lab5_6/Task1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+int main() {
+unsigned cpu, node;
+// Get current CPU core and NUMA node via system call
+// Note this has no glibc wrapper so we must call it directly
+syscall (SYS_getcpu, &cpu, &node, NULL);
+printf("This program is running on CPU core %u and NUMA node print%u.\n\n",cpu,node);
+return 0;
+}
